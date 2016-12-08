d471abe6c2009f99589df893a7bce43e951eef8b
Geza Csenger <csengergeza@gmail.com>
2016-12-08 21:02:10 +0100

    add pug view engine

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var express = require('express')
 var app = express()
 
+app.set('view engine', 'pug')
+
 app.get('/', function (req, res) {
-	console.log('GET request on /')
-	res.send('Hello World!')
+    console.log('GET request on /')
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
 app.listen(3000, function () {
