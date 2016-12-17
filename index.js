4559d061d0d402d8187f5370dbe89fb37c46de32
András Eisenberger <csirkeee@gmail.com>
2016-12-17 17:55:43 +0100

    Log POST action to console

--- index.js
+++ index.js
@@ -2,9 +2,20 @@ var express = require('express')
 var app = express()
 
 app.set('view engine', 'pug')
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+}));
+
 
 app.get('/', function (req, res) {
     console.log('GET request on /')
+    res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
+app.post('/action', function (req, res) {
+    console.log('POST /action with name ' + req.body.firstname)
     res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
