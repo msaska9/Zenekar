2d834a84b3b1bab4ae8c2da61400b6c89bf26047
Fraknoi Adam <fraknoiadam@gmail.com>
2016-12-17 19:07:52 +0100

    Adatbázis létrehozás

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express')
 var app = express()
+var dbapi = require('./db.js')
+var maindb = new dbapi.database("mydb.db")
 
 app.set('view engine', 'pug')
 var bodyParser = require('body-parser')
@@ -16,9 +18,13 @@ app.get('/', function (req, res) {
 
 app.post('/action', function (req, res) {
     console.log('POST /action with name ' + req.body.firstname)
+    var adat=req.body.firstname
+    maindb.query("INSERT INTO user_info (info) VALUES (?)", null, [[adat]])
     res.render('index', { title: 'Hey', message: 'Hello there!' })
 })
 
 app.listen(3000, function () {
+  maindb.query("CREATE TABLE if not exists user_info (info TEXT)", null, [[]])
 	console.log('Example app listening on port 3000!')
 })
+
