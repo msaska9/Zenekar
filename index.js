35420a703ae493b92d30fa2c5d887f63d464d765
Geza Csenger <csengergeza@gmail.com>
2017-01-30 15:33:12 -0600

    Passport.js mukodo

--- index.js
+++ index.js
@@ -3,146 +3,133 @@ var app = express();
 
 app.set('view engine', 'pug');
 
-// require
-var dbapi = require('./db.js');
-var maindb = new dbapi.database("mydb.db");
+// REQUIRE
+
+// adatbazis
+var dbapi = require('./db.js'); // api
+var maindb = new dbapi.database("mydb.db"); // adatbazis fajlban
+
+// passport
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
+
+// sessions, parsers
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
-var md5 = require('js-md5');
 var bodyParser = require('body-parser');
 
-// middleware config
-app.use(express.static('public'));
-app.use(cookieParser());
+var md5 = require('js-md5');
+
+// middleware config - parsers
 app.use(bodyParser());
+app.use(bodyParser.urlencoded({ // support URL-encoded bodies
+  extended: true
+}));
+app.use(bodyParser.json()); // support JSON
+app.use(cookieParser());
+
+app.use(express.static('public'));
 app.use(session({
   secret: 'KEK LOL ROFL HAHA LUL WOW OMG LMAO'
 }));
+
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(bodyParser.json()); // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
-  extended: true
-}));
 
-// itt írjuk meg a saját stratégiánkat, ami szerint eldöntjük, hogy beléphet-e a
-// user vagy sem
+
+// sajat stratégiank
+// a bejelentkezes e-maillel es jelszoval tortenik
 passport.use(new LocalStrategy(
-  function (email, pw, done) {
-    var given_email = email;
-    var given_password = pw;
-    console.log('kukucs');
-    maindb.query("SELECT * FROM user WHERE email=?", function (err, result) {
-      if (result.length == 0) {
-        console.log('No such email');
-        return done(null, false);
-      } else {
-        var userdata = result[0];
-        if (md5(given_password) != userdata.password) { // ha rossz a jelszó
-          console.log('Bad password');
-          return done(null, false); // ezt a függvényt kell hívni a dokumentáció szerint
-        }
-        if (md5(given_password) == userdata.password) { // ha jó a jelszó, akkor minden OK
-          console.log("New login");
-          return done(null, userdata); // továbbküldjük a user változót
-        }
-      }
-    }, [
-      [given_email]
-    ])
+	// kulon megmondjuk, hogy mik a fieldeink
+	{
+		usernameField: 'email', 
+		passwordField: 'password',
+	}, function (username, password, done) { // a strategia fuggvenye
+		maindb.query("SELECT * FROM user WHERE email=?", function (err, result) {
+			if ( result.length == 0 ) { // ha nem talaltunk usert
+				console.log('No such user'); 
+				return done(null, false);
+		  	} else {
+				var user = result[0]; // eltaroljuk a user adatait
+				if ( md5(password) != user.password ) { // ha rossz a jelszó
+					console.log('Bad password');
+					return done(null, false); // ezt a függvényt kell hívni a dokumentáció szerint
+				}
+
+				if ( md5(password) == user.password ) { // ha jó a jelszó, akkor minden OK
+					console.log("New login");
+					return done(null, user); // továbbküldjük a user változót
+				}
+		  	}
+    	}, [[username]])
   }
 ));
 
-// elmentjük a user-t a sessionbe
+// user hozzaadasa a sessionbe
 passport.serializeUser(function (user, done) {
-  done(null, user); // user elmentése
+	done(null, user); // user elmentése
 });
 
-// ha pl. kijelentkezik, akkor nincs már szükség arra, hogy
-// az adatai továbbra is a sessionben legyenek tárolva
+// user torlese a sessionbol
 passport.deserializeUser(function (user, done) {
-  done(err, user);
+	done(null, user);
 });
 
 function checkLogin(req, res, next) { // a saját checkLogin middleware függvényünk
-  if (req.user) { // a user be van jelentkezve, van joga látni a titkos oldalt
-    console.log('user is logged in');
-    res.redirect('/secret_page'); // átirányítjuk a titkos oldalra
-  } else { // a user nincs bejelentkezve
-    console.log('user is not logged in');
-    res.redirect('/signin'); // átirányítjuk a bejelentkezéshez
-  }
-  next();
-}
+	if (req.user) { // a user be van jelentkezve, van joga látni a titkos oldalt
+		console.log('user is logged in');
+		res.redirect('/secret_page'); // átirányítjuk a titkos oldalra
+	} else { // a user nincs bejelentkezve
+  		console.log('user is not logged in');
+		res.redirect('/signin'); // átirányítjuk a bejelentkezéshez
+	}
 
-function output(err, res) {
-  for (var i = 0; i < res.length; i++) {
-    console.log(res[i].info);
-  }
+	next();
 }
-
-/*app.get('/', function (req, res) {
-  console.log('GET request on /')
-  res.render('index')
-})*/
 
 app.get('/signin', function (req, res) {
-  console.log('GET request on /signin');
-  res.render('signin');
+	console.log('GET request on /signin');
+	res.render('signin');
 });
 
 app.get('/signup', function (req, res) {
-  console.log('GET request on /signup');
-  res.render('signup');
+	console.log('GET request on /signup');
+	res.render('signup');
 });
 
-app.post('/signin', passport.authenticate('local', { successRedirect: '/', failureRedirect: '/signin' }));
+// ez a passport middleware fuggvenye a bejelentkezeshez
+var auth = passport.authenticate('local', { successRedirect: '/', failureRedirect: '/signin' });
+app.post('/signin', auth);
 
 app.post('/signup', function (req, res) {
-  var adat = Object();
-  adat.firstname = req.body.firstname;
-  adat.lastname = req.body.lastname;
-  adat.email = req.body.email;
-  adat.pw = req.body.pw;
-  adat.pw = md5(adat.pw);
-  maindb.query("INSERT INTO user (firstname, lastname, email, password) VALUES (?, ?, ?, ?)", function (err, result) {
-    res.render('signup');
-  }, [
-    [adat.firstname, adat.lastname, adat.email, adat.pw]
-  ]);
+	var adat = Object();
+	adat.firstname = req.body.firstname;
+	adat.lastname = req.body.lastname;
+	adat.email = req.body.email;
+	adat.pw = req.body.pw;
+	adat.pw = md5(adat.pw);
+
+	maindb.query("INSERT INTO user (firstname, lastname, email, password) VALUES (?, ?, ?, ?)", function (err, result) {
+		res.render('signup');
+	}, [[adat.firstname, adat.lastname, adat.email, adat.pw]]);
 });
 
 
 app.get('/', checkLogin); // használjuk a middlewaret, ha a /-re jön egy request
 
-
-/*app.post('/action', function (req, res) {
-    console.log('POST /action with name ' + req.body.firstname)
-    var adat=req.body.firstname
-    maindb.query("INSERT INTO user (info) VALUES (?)", null, [[adat]])
-    maindb.query("SELECT info FROM user_info", function(err, res){output(err, res)}, [[]]);
-    res.render('index')
-})*/
-
 app.get('/adatbazis', function (req, res) {
-  var tomb = Array();
-  maindb.query("SELECT * FROM user", function (err, result) {
-    for (var i = 0; i < result.length; i++) {
-      tomb.push(result[i].firstname + " " + result[i].lastname + " " + result[i].email + " " + result[i].password);
-    }
-    res.render('adatbazis', {
-      user: tomb
-    })
-  }, [
-    []
-  ])
+	var tomb = Array();
+
+	maindb.query("SELECT * FROM user", function (err, result) {
+		for (var i = 0; i < result.length; i++) {
+    		tomb.push(result[i].firstname + " " + result[i].lastname + " " + result[i].email + " " + result[i].password);
+		}
+
+		res.render('adatbazis', { user: tomb });
+	}, [[]])
 })
 
 app.listen(3000, function () {
-  maindb.query("CREATE TABLE if not exists user (firstname TEXT, lastname TEXT, email TEXT, password TEXT)", null, [
-    []
-  ])
-  console.log('Example app listening on port 3000!')
+	maindb.query("CREATE TABLE if not exists user (firstname TEXT, lastname TEXT, email TEXT, password TEXT)", null, [[]])
+	console.log('Example app listening on port 3000!')
 })
